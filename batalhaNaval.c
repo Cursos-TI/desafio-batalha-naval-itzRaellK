9c74b6f44f999c1af44129dfe1b974f792019615
Israel Kessler Ramos <86384892+itzRaellK@users.noreply.github.com>
2025-01-23 01:28:12 +0000

    Criei sistema para printar posição dos navios

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,35 +6,32 @@
 
 int main() {
     // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    // DEFININDO AS VARIÁVEIS DE CONTROLE
+    int posicao_linha, posicao_coluna;
+    int linha = 5;
+    int coluna = 5;
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    // DEFININDO A MATRIZ - 0 = POSIÇÃO NEGATIVA / 1 = POSIÇÃO POSITIVA
+    int tabuleiro[5][5] = {
+        {0,0,0,0,0},
+        {1,1,1,0,0},
+        {0,0,0,0,1},
+        {0,0,0,0,1},
+        {0,0,0,0,1}
+    };
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    // MENSAGEM ADEQUADA PARA INÍCIO DO DESAFIO
+    printf("\n\n-------------------------------- DESAFIO BATALHA NAVAL --------------------------------\n\n");
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    // UM LOOP (REPETIÇÃO) PARA EXIBIR CADA POSIÇÃO DA MATRIZ E REVELANDO A POSIÇÃO DOS NAVIOS NA TELA DO USUÁRIO
+    for (posicao_linha = 0; posicao_linha < linha; posicao_linha++) {
+        for (posicao_coluna = 0; posicao_coluna < coluna; posicao_coluna++){
+            printf("| %d ", tabuleiro[posicao_linha][posicao_coluna]);
+        }
+        printf("|");
+        printf("\n");
+    }
 
     return 0;
 }
